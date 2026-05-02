fffd80053205cdd5d9cfd74bd2bd0b0b50eb81a3
wpalkaa <poukoxy@gmail.com>
2026-05-02 15:15:22 +0200

    kod_old

--- kod.py
+++ kod.py
@@ -1,29 +1,42 @@
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
 df = pd.read_csv('data/processed/Ship_operation.csv')
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', 1000)
+
 
 # print(df.iloc[(df["acc"] > 0.05) | (df["acc"] < -0.05)])
 # outliers = df[(df["acc"] > 0.03) | (df["acc"] < -0.03)]
 # print(len(outliers))
 # print(df["acc"].quantile([0.01, 0.05, 0.95, 0.99]))
 
+frame = df[df["is_outlier"] != 1]
+plt.show()
+#signaldate,LAT,LON,is_outlier,COG,traveled,dt,velocity_m_s,velocity_knot,is_drift,dVelocity_m_s,dCOG,acc,omega,is_first_in_segment,segment_id
+print(frame[["is_outlier", "LAT", "LON", "traveled", "dt", "velocity_m_s", "is_drift", "COG", "dCOG", "acc"]].describe())
+print(frame.loc[frame.isna().any(axis=1)])
 
-col = "velocity_m_s"
+# col = "velocity_m_s"
 
-Q1 = df[col].quantile(0.25)
-Q3 = df[col].quantile(0.75)
-IQR = Q3 - Q1
+# Q1 = df[col].quantile(0.25)
+# Q3 = df[col].quantile(0.75)
+# IQR = Q3 - Q1
 
-dolna_granica = Q1 - 1.5 * IQR
-gorna_granica = Q3 + 1.5 * IQR
-print(dolna_granica)
-print(gorna_granica)
+# dolna_granica = Q1 - 1.5 * IQR
+# gorna_granica = Q3 + 1.5 * IQR
+# print(dolna_granica)
+# print(gorna_granica)
 
-df = df[(df["velocity_m_s"] >= dolna_granica) & (df["velocity_m_s"] <= gorna_granica)]
-print(df.describe())
+# df = df[(df["velocity_m_s"] >= dolna_granica) & (df["velocity_m_s"] <= gorna_granica)]
+# print(df.describe())
+# print(df.columns.tolist())
 
-# import matplotlib.pyplot as plt
-# import numpy as np
-# import seaborn as sns
+# print(df.iloc[df["is_first_in_segment"] == 1])
+
+
 
 
 # # print(df.describe())
--- src/data/preprocessing.py
+++ src/data/preprocessing.py
@@ -9,58 +9,101 @@ class DataPreprocessor:
     Cleans and transforms raw GPS data into kinematic features
     including distance, velocity, acceleration, and course over ground.
     """
-    def __init__(self, drop_prev: bool = True, drift_threshold: int = 0.51444, clean_data: bool = True):
+    def __init__(
+        self, 
+        drop_prev: bool = True, 
+        drift_threshold: int = 0.51444,
+        max_speed_m_s: int = 12.86, 
+        max_acc_m_s2: int = 0.05
+        ):              
         self.drop_prev = drop_prev
         self.drift_threshold = drift_threshold
+        self.max_speed_m_s = max_speed_m_s
+        self.max_acc_m_s2 = max_acc_m_s2
 
     def process(self, df: pd.DataFrame) -> pd.DataFrame:
         """
-        Run pipeline method.
+        Execute the preprocessing pipeline.
         """
         return (
             df.copy()
-            .pipe(self._clean_data)
-            .pipe(self._calculate_kinematics)
-            .pipe(self._handle_outliers_and_drift)
+            .pipe(self._gps_find_outliers)
+            .pipe(self._handle_kinematics)
+            .pipe(self._segment_data)
+            .pipe(self._fill_first_lines_nans)
         )
     
     # ============ DATA CLEANING ============
     
-    def _clean_data(self, frame: pd.DataFrame) -> pd.DataFrame:
+    def _fill_first_lines_nans(self, frame: pd.DataFrame) -> pd.DataFrame:
+        frame.iloc[0:2] = frame.iloc[0:2].fillna(0.0)
+        
+        return frame
+    
+    def _gps_find_outliers(self, frame: pd.DataFrame) -> pd.DataFrame:
         """
-        Cleans data by removing invalid coordinates, duplicates and sorting by time. 
-        Flags first record.
+        Cleans data by flagging invalid coordinates, duplicates and sorting by time as outliers. 
         """
+        
+        print("========== WYSZUKIWANIE OUTLIEROW Z GPS ==========")
+        
+        frame["is_outlier"] = 0
+        
+        # Sort
+        frame = frame.sort_values(["signaldate"])
         
         # If NaN in GPS => remove
-        frame = frame.dropna(subset=["LAT", "LON"]).reset_index(drop=True)
+        nan_mask = frame["LAT"].isna() | frame["LON"].isna()
+        nan_sum = nan_mask.sum()
+        frame.loc[nan_mask, "is_outlier"] = 1
+        print(f"[Info]: Nowych outlier'ów, przez braki danych GPS (NaN): {nan_sum}")
         
         # Drop invalid GPS 
-        frame = frame[frame["LAT"].between(-90, 90) & frame["LON"].between(-180, 180)]
-        frame = frame[(frame["LAT"] != 0) & (frame["LON"] != 0)]
+        range_mask = frame["LAT"].between(-90, 90) & frame["LON"].between(-180, 180)
+        wrong_range_sum = (~range_mask & (frame["is_outlier"] == 0)).sum()
+        frame.loc[~range_mask, "is_outlier"] = 1
+        print(f"[Info]: Nowych outlier'ów, przez GPS poza zakresem (-90, 90) ; (-180, 180): {wrong_range_sum}")
         
-        # Clean date
+        # Point (0, 0)
+        zero_point_mask = (frame["LAT"] == 0) & (frame["LON"] == 0)
+        zero_point_sum = (zero_point_mask & (frame["is_outlier"] == 0)).sum()
+        frame.loc[zero_point_mask, "is_outlier"] = 1
+        print(f"[Info]: Nowych outlier'ów, przez rekordy w punkcie (0, 0): {zero_point_sum}")
+        
+        # Duplicates
         frame["signaldate"] = pd.to_datetime(frame["signaldate"])
-        frame = frame.drop_duplicates(subset=["signaldate"])
-        frame = frame.sort_values(["signaldate"]).reset_index(drop=True)
+        dupe_mask = frame.duplicated(subset=["signaldate"], keep="first")
+        dupe_sum = (dupe_mask & (frame["is_outlier"] == 0)).sum()
+        frame.loc[dupe_mask, "is_outlier"] = 1
+        print(f"[Info]: Nowych outlier'ów, przez duplikaty dat: {dupe_sum}")
         
-        # Is first row
-        frame.insert(0, "is_first", frame.index == 0)
-            
+        # GPS freeze
+        freeze_mask = (frame["LAT"] == frame["LAT"].shift(1)) & (frame["LON"] == frame["LON"].shift(1))
+        freeze_sum = (freeze_mask & (frame["is_outlier"] == 0)).sum()
+        frame.loc[freeze_mask, "is_outlier"] = 1
+        print(f"[Info]: Nowych outlier'ów, przez GPS freeze: {freeze_sum}")
+        
+        # Summary
+        print(f"[Info]: Wykryto łącznie: {(frame["is_outlier"] == 1).sum()} outlierów.")
+        
         return frame
 
     # ============ KINEMATICS PIPELINE ============
 
-    def _calculate_kinematics(self, frame: pd.DataFrame) -> pd.DataFrame:
+    def _handle_kinematics(self, frame: pd.DataFrame) -> pd.DataFrame:
         """
         Computes movement metrics: distance, time delta, velocity, acceleration and COG.
         """
+        print(f"========== KINEMATYKA ==========")
+        
         frame = self._add_prev_values(frame)
         frame = self._calculate_distance_dtime(frame)
         frame = self._calculate_velocity(frame)
         frame = self._calculate_COG(frame)
-        frame = self._remove_drift_noise(frame)
+        frame = self._mark_unnatural_velocity(frame)
+        frame = self._mark_drift(frame)
         frame = self._calculace_kinematics_params(frame)
+        frame = self._mark_unnatural_acceleration(frame)
 
         if self.drop_prev:
             frame = self._drop_helper_columns(frame)
@@ -71,57 +114,72 @@ class DataPreprocessor:
         """
         Calculates movement metrics parameters: dVelocity, acceleration, dCOG.
         """
-        # Velocity diff
-        frame["prev_velocity_m_s"] = frame["velocity_m_s"].shift(1)
-        frame["dVelocity_m_s"] = frame["velocity_m_s"] - frame["prev_velocity_m_s"]
+        mask = frame["is_outlier"] == 0
         
-        # COG diff
-        frame["prev_COG"] = frame["COG"].shift(1)
-        frame["dCOG"] = (frame["COG"] - frame["prev_COG"] + 180) % 360 - 180 # normalization (-180* : 180*)
-        frame["dCOG"] = frame["dCOG"].fillna(0.0)
+        # # Velocity diff
+        frame.loc[mask, "prev_velocity_m_s"] = frame.loc[mask, "velocity_m_s"].shift(1)
+        frame.loc[mask, "dVelocity_m_s"] = frame.loc[mask, "velocity_m_s"] - frame.loc[mask, "prev_velocity_m_s"]
         
-        # Acceleration
-        frame["acc"] = frame["dVelocity_m_s"] / frame["dt"]
-    
+        # # COG diff
+        frame.loc[mask, "prev_COG"] = frame.loc[mask, "COG"].shift(1)
+        frame.loc[mask, "dCOG"] = (frame.loc[mask, "COG"] - frame.loc[mask, "prev_COG"] + 180) % 360 - 180 # normalization (-180* : 180*)
+        frame.loc[mask, "dCOG"] = frame.loc[mask, "dCOG"].fillna(0.0)
+        
+        # # Acceleration
+        frame.loc[mask, "acc"] = frame.loc[mask, "dVelocity_m_s"] / frame.loc[mask, "dt"]
+        
+        # # Angular Velocity
+        frame.loc[mask, "omega"] = frame.loc[mask, "dCOG"] / frame.loc[mask, "dt"]
+
         return frame
         
     
     # Add previous LAT, LON and signaldate
     def _add_prev_values(self, frame: pd.DataFrame) -> pd.DataFrame:
-        frame["prev_LAT"] = frame["LAT"].shift(1)
-        frame["prev_LON"] = frame["LON"].shift(1)
-        frame["prev_signaldate"] = frame["signaldate"].shift(1)
+        mask = frame["is_outlier"] == 0
+            
+        frame.loc[mask, "prev_LAT"] = frame.loc[mask, "LAT"].shift(1)
+        frame.loc[mask, "prev_LON"] = frame.loc[mask, "LON"].shift(1)
+        frame.loc[mask, "prev_signaldate"] = frame.loc[mask, "signaldate"].shift(1)
+        
         
         return frame
 
     # Calculate traveled distance in meters
-    def _calculate_distance_dtime(self, frame: pd.DataFrame) -> pd.DataFrame:
-        frame["traveled"] = haversine(
-            frame["prev_LAT"].values,
-            frame["prev_LON"].values,
-            frame["LAT"].values,
-            frame["LON"].values
-        )
+    def _calculate_distance_dtime(self, frame: pd.DataFrame) -> pd.DataFrame:        
+        mask = frame["is_outlier"] == 0
 
-        frame["dt"] = (frame["signaldate"] - frame["prev_signaldate"]).dt.total_seconds()
+        frame["COG"] = np.nan 
+
+        frame.loc[mask, "traveled"] = haversine(
+            frame.loc[mask, "prev_LAT"].values,
+            frame.loc[mask, "prev_LON"].values,
+            frame.loc[mask, "LAT"].values,
+            frame.loc[mask, "LON"].values
+        )
+        
+        frame.loc[mask, "dt"] = (frame.loc[mask, "signaldate"] - frame.loc[mask, "prev_signaldate"]).dt.total_seconds()
 
         return frame
     
     # Calculate COG
     def _calculate_COG(self, frame: pd.DataFrame) -> pd.DataFrame:
-        frame["COG"] = calc_course(
-            frame["prev_LAT"].values,
-            frame["prev_LON"].values,
-            frame["LAT"].values,
-            frame["LON"].values
+        mask = frame["is_outlier"] == 0
+        frame.loc[mask, "COG"] = calc_course(
+            frame.loc[mask, "prev_LAT"].values,
+            frame.loc[mask, "prev_LON"].values,
+            frame.loc[mask, "LAT"].values,
+            frame.loc[mask, "LON"].values
         )
     
         return frame
         
 
-    def _calculate_velocity(self, frame: pd.DataFrame) -> pd.DataFrame:
-        frame["velocity_m_s"] = frame["traveled"] / frame["dt"]
-        frame["velocity_knot"] = frame["velocity_m_s"] * 1.94384
+    def _calculate_velocity(self, frame: pd.DataFrame) -> pd.DataFrame:        
+        mask = frame["is_outlier"] == 0
+        
+        frame.loc[mask, "velocity_m_s"] = frame.loc[mask, "traveled"] / frame.loc[mask, "dt"]
+        frame.loc[mask, "velocity_knot"] = frame.loc[mask, "velocity_m_s"] * 1.94384
         
         return frame
     
@@ -138,40 +196,67 @@ class DataPreprocessor:
     
     
     
-    # ============ OUTLIERS & DRIFT ============
-    def _remove_drift_noise(self, frame: pd.DataFrame) -> pd.DataFrame:
-        # Noise filter : velocity < drift_threshold
-        frame["is_drift"] = (frame["velocity_m_s"] < self.drift_threshold).astype(int)
-
-        # If drifing, set velocity to 0, course = NaN
-        is_drift = frame["is_drift"] == 1
-        frame.loc[is_drift, ["velocity_m_s", "velocity_knot", "traveled"]] = 0.0
-        frame.loc[is_drift, ["COG"]] = np.nan
+    # ============ DRIFT ============   
+    def _mark_drift(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """
+        Marks if ship is drifting.
+        """
+        frame["is_drift"] = 0
+        mask = frame["is_outlier"] == 0
+        
+        drift_mask = frame["velocity_m_s"] < self.drift_threshold
+        drift_sum = (mask & drift_mask).sum()
+        frame.loc[mask & drift_mask, "is_drift"] = 1
+        
+        print(f"[Info]: Wykryto {drift_sum} statków dryfujących.")
         
         return frame 
-        
-    def _handle_outliers_and_drift(self, frame: pd.DataFrame) -> pd.DataFrame:
+         
+    def _mark_unnatural_velocity(self, frame: pd.DataFrame) -> pd.DataFrame:
         """
-        Removes outliers from data using IQR and flags GPS drift.
+        Marks outlier if velocity is unnatural.
         """
+        mask = frame["is_outlier"] == 0
         
-        columns_to_check = ["velocity_m_s"]
+        unnatural_mask = frame["velocity_m_s"] > self.max_speed_m_s
+        unnatural_sum = (unnatural_mask & mask).sum()
+        frame.loc[unnatural_mask & mask, "is_outlier"] = 1
         
-        for col in columns_to_check:
-            Q1 = frame[col].quantile(0.25)
-            Q3 = frame[col].quantile(0.75)
-            IQR = Q3 - Q1
-            
-            lower_bound = Q1 - 1.5 * IQR 
-            upper_bound = Q3 + 1.5 * IQR
-            
-            frame = frame[frame[col].between(lower_bound, upper_bound)]
+        print(f"[Info]: Statki z nienaturalną prędkością: {unnatural_sum}")
+        
+        return frame
+        
+    def _mark_unnatural_acceleration(self, frame: pd.DataFrame) -> pd.DataFrame:
+        """
+        Marks outlier if acceleration is unnatural.
+        """
+        mask = frame["is_outlier"] == 0
+        unnatural_mask = (frame["acc"] > self.max_acc_m_s2) | (frame["acc"] < -self.max_acc_m_s2)
+        unnatural_sum = (unnatural_mask & mask).sum()
+        frame.loc[unnatural_mask & mask, "is_outlier"] = 1
+        
+        print(f"[Info]: Statki z nienaturalnym przyspieszeniem: {unnatural_sum}")
+        
+        return frame
+        
+    # ============ SEGMENT DATA ============
+    def _segment_data(self, frame: pd.DataFrame) -> pd.DataFrame:
+        mask = frame["is_outlier"] == 0
+        
+        first_valid_idx = frame.index[mask][0]
+        is_new_segment = mask & ((frame["dt"] > 300) | (frame.index == first_valid_idx))
 
-        # Uśrednia pozycję z 3 ostatnich minut, wygładzając skoki GPS
-        # a = frame["LAT"].rolling(window=3, center=True, min_periods=1).mean()
-        # b = frame["LON"].rolling(window=3, center=True, min_periods=1).mean()
-        
-        # frame.insert(3, "LAT_smooth", a)
-        # frame.insert(4, "LON_smooth", b)
+        cols_to_zero = ["velocity_m_s", "velocity_knot", "dVelocity_m_s", "dCOG", "acc"]
+        frame.loc[is_new_segment, cols_to_zero] = 0.0
+        frame.loc[is_new_segment, "is_drift"] = 0
 
+        frame.loc[is_new_segment, "COG"] = np.nan
+        frame.loc[mask, "COG"] = frame.loc[mask, "COG"].ffill().fillna(0.0)
+
+        frame["is_first_in_segment"] = 0
+        frame.loc[is_new_segment, "is_first_in_segment"] = 1
+
+        frame["segment_id"] = is_new_segment.cumsum()
+        frame.loc[~mask, "segment_id"] = -1
+        
         return frame
